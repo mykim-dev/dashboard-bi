53425563f072c87e5dc6ca8004ec99d959b19c0a
mykim-dev <pandagom79@gmail.com>
2023-01-18 21:51:11 +0900

    console test

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './assets/styles/index.scss'
 
 const app = createApp(App)
